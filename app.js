902dd48885368d7162bfc470838ee8faf7703fc5
Metin Karabulut <114526516+MetinKb@users.noreply.github.com>
2024-06-14 16:24:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,9 +9,6 @@ const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
 
 bot.command('reels', async ctx => {
     try {
-        /* We do chat/group control because we don't want +18 posts 
-        to be downloaded and posted to the group using bots. 
-        If the command is run in group it will send the warning message below. */
         const chatType = ctx.chat.type
         if (chatType !== 'private') {
             bot.telegram.sendMessage(
@@ -20,14 +17,9 @@ bot.command('reels', async ctx => {
             )
             return
         }
-
-        /* We get the video link to be sent with the /reels command
-        and we define the folder where the videos will be downloaded and sent. */
+        
         const reelsLink = ctx.message.text.split(' ')[1]
         const reelsFolderPath = './reels'
-        console.log(reelsLink)
-        /* I used an api from the Rapid API site. You may have to make minor changes in the code according 
-        to the response data returned by the API you will use. You will need a API key for these operations. */
         const options = {
             method: 'GET',
             url: 'process.env.API_URL',
@@ -40,9 +32,6 @@ bot.command('reels', async ctx => {
             },
         }
 
-        /* We take the video from the response data returned with the axios library 
-        and save it as mp4 in the reels folder. And we send the message to the user that 
-        the video is downloading until the video is downloaded. */
         const response = await axios(options)
         const videoUrl = response.data.media
         const outputFilePath = path.join(reelsFolderPath, `${ctx.botInfo.username}.mp4`)
@@ -57,13 +46,10 @@ bot.command('reels', async ctx => {
         mediaResponse.data.pipe(outputStream)
         const downloadingMessage = await ctx.reply('The video is downloading...')
 
-        /* Delete 'Downloading video...' and send 'Sending video...' when video is downloaded
-        codes that will run when the file is successfully downloaded */
         outputStream.once('finish', async () => {
             bot.telegram.deleteMessage(ctx.chat.id, downloadingMessage.message_id)
             const sendingMessage = await ctx.reply('The video is sending...')
 
-            //  We send the video to the user and delete the video after sending.
             bot.telegram
                 .sendVideo(ctx.message.chat.id, { source: outputFilePath })
                 .then(() => {
@@ -84,14 +70,11 @@ bot.command('reels', async ctx => {
                 })
         })
 
-        /* If an error occurs while downloading the video, we delete the 'video downloading...' message 
-        and send the message 'An error occurred while downloading the video*/
         outputStream.on('error', error => {
             console.error(error)
             bot.telegram.deleteMessage(ctx.chat.id, downloadingMessage.message_id)
             ctx.reply('An error occurred while downloading the video.')
         })
-        // if reels link is not entered, give a warning
     } catch (error) {
         ctx.reply('Please enter a valid real link.')
         throw error
